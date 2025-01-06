f04dd01e18fd58f0c29c7a19d52dcd58f93f4ec1
Katiaryna Dounar <ekaterinaaleks9@gmail.com>
2025-01-06 16:31:18 +0300

    feat: add solution to task vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (isDirect) {
+    this.isDirect = isDirect
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (arguments.length < 2 || message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let cypher = '';
+    let index = 0;
+    for (let i = 0; i < message.length; i++) {
+      const letter = message[i].toUpperCase();
+      if (alphabet.includes(letter)) {
+        const charIndex = alphabet.indexOf(letter);
+        const keyLetterIndex = alphabet.indexOf(key[index % key.length].toUpperCase());
+        let newIndex = (charIndex + keyLetterIndex) % 26;
+        cypher += alphabet[newIndex];
+        index++;
+      } else {
+        cypher += letter;
+      }
+    }
+    if (this.isDirect === false) {
+      return cypher.split('').reverse().join('');
+    } else {
+      return cypher;
+    }
+  }
+  decrypt(encryptedMessage, key) {
+    if (arguments.length < 2 || encryptedMessage === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let cypher = '';
+    let index = 0;
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      const letter = encryptedMessage[i].toUpperCase();
+      if (alphabet.includes(letter)) {
+        const letterIndex = alphabet.indexOf(letter);
+        const keyLetterIndex = alphabet.indexOf(key[index % key.length].toUpperCase());
+        let newIndex = (letterIndex - keyLetterIndex + 26) % 26;
+        cypher += alphabet[newIndex];
+        index++;
+      } else {
+        cypher += letter;
+      }
+    }
+    if (this.isDirect === false) {
+      return cypher.split('').reverse().join('');
+    } else {
+      return cypher;
+    }
   }
 }
 
