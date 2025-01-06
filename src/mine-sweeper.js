f1e005a262e0b8d2e475353e3c5b298c6f47c0fa
Katiaryna Dounar <ekaterinaaleks9@gmail.com>
2025-01-06 14:35:50 +0300

    feat: add solution to task mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let x = matrix.length;
+  let y = matrix[0].length;
+  let resultArr = Array(x).fill().map(() => Array(y).fill(0));
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < y; j++) {
+      let count = 0;
+      if (i > 0 && matrix[i - 1][j]) count++; //верхняя ячейка
+      if (i < x - 1 && matrix[i + 1][j]) count++; //нижняя ячейка
+      if (j > 0 && matrix[i][j - 1]) count++; // левая ячейка
+      if (j < y - 1 && matrix[i][j + 1]) count++; // правая ячейка
+      if (i > 0 && j > 0 && matrix[i - 1][j - 1]) count++; // верх-лево
+      if (i > 0 && j < y - 1 && matrix[i - 1][j + 1]) count++; // верх-право
+      if (i < x - 1 && j > 0 && matrix[i + 1][j - 1]) count++; // низ-лево
+      if (i < x - 1 && j < y - 1 && matrix[i + 1][j + 1]) count++; // низ-право
+      resultArr[i][j] = matrix[i][j] ? 1 : count;
+    }
+  }
+  return resultArr;
 }
 
 module.exports = {
